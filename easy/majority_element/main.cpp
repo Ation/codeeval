84246c534de4cfc8f4ce42ae58882f6aae0b46e4
Ation <gation@gmail.com>
2014-03-17 23:28:46 +0200

    Add base file for solution

---
+++ easy/majority_element/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        return 0;
+    }
+
+    string inputLine;
+    ifstream inFile(argv[1]);
+
+    if ( ! inFile.is_open() ) {
+        return 1;
+    }
+
+    while ( getline(inFile, inputLine)) {
+        
+    }
+
+    return 0;
+}
---
+++ easy/majority_element/readme.txt
@@ -0,0 +1,4 @@
+Solution to "Major element" challange
+https://www.codeeval.com/open_challenges/132/
+
+This is an example of Moore's linear time majority vote algorithm
