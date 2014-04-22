85ed1039c0c3d6e2b4bcd45060907feebe53103b
Ation <gation@gmail.com>
2014-04-22 19:34:08 +0300

    Update solution for stairs

--- hard/stairs/main.cpp
+++ hard/stairs/main.cpp
@@ -17,8 +17,7 @@ unsigned int getInteger(const string &line) {
     return result;
 }
 
-unsigned long long getFibonacci(const string &line) {
-    unsigned int index = getInteger(line);
+unsigned long long getFibonacci(unsigned int index) {
     unsigned long long number = 0;
 
     unsigned long long left;
@@ -46,66 +45,11 @@ unsigned long long getFibonacci(const string &line) {
 }
 
 unsigned long long getCombinationsCount(const string &line) {
-    unsigned long long result = 0;
-
     unsigned int stairs_cout = getInteger(line);
-    unsigned long long fib = getFibonacci(stairs_cout + 1);
-
-    unsigned int two_count = stairs_cout / 2;
-    unsigned int one_count = stairs_cout & 1;
-
-    unsigned long long last_group;
-
-    if (one_count == 0) {
-        result = last_group = 1;
-    } else {
-        result = last_group = one_count + two_count;
-    }
-
-    while (two_count != 0) {
-        last_group *= two_count;
-
-        one_count ++;
-        last_group *= (one_count + two_count);
-
-        two_count--;
-        last_group /= one_count;
-        one_count++;
-        last_group /= one_count;
-
-        result += last_group;
-    }
-
-    if (result != fib) {
-        cout << fib << ':' << result << endl;
-    } else {
-        cout << result << endl;
-    }
-
-    return result;
+    return getFibonacci(stairs_cout + 1);
 }
 
 int main(int argc, char *argv[]) {
-    getCombinationsCount("774");
-    getCombinationsCount("564");
-    getCombinationsCount("528");
-    getCombinationsCount("28");
-    getCombinationsCount("352");
-    getCombinationsCount("22");
-    getCombinationsCount("205");
-    getCombinationsCount("937");
-    getCombinationsCount("875");
-    getCombinationsCount("253");
-    getCombinationsCount("836");
-    getCombinationsCount("686");
-    getCombinationsCount("10");
-    getCombinationsCount("148");
-    getCombinationsCount("392");
-    getCombinationsCount("250");
-    getCombinationsCount("467");
-    getCombinationsCount("852");
-    getCombinationsCount("999");
-    getCombinationsCount("162");
 
     if (argc != 2) {
         return 0;
@@ -119,7 +63,7 @@ int main(int argc, char *argv[]) {
     }
 
     while ( getline(inFile, inputLine)) {
-        getCombinationsCount(inputLine);
+        cout << getCombinationsCount(inputLine) << endl;
     }
 
     return 0;
