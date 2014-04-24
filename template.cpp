4a783eaaef7e5085417f3659e1f67d660b77a928
Ation <gation@gmail.com>
2014-04-24 15:47:43 +0300

    Add getUnsignedInteger function to template

--- template.cpp
+++ template.cpp
@@ -7,6 +7,22 @@
 
 using namespace std;
 
+unsigned int getUnsignedInteger(const string &line, size_t &position) {
+    int result = 0;
+    char temp;
+    for (; position < line.length(); position++) {
+        temp = line[position];
+        if (temp >= '0' && temp <= '9') {
+            result = result * 10 + (temp - '0');
+        } else {
+            position ++;
+            break;
+        }
+    }
+
+    return result;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         return 0;
