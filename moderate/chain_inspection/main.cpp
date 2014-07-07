76b3751b24c714ab99e44266fa0f83b4ebdb8f49
Ation <gation@gmail.com>
2014-07-07 12:21:37 +0300

    Fixed solution to 'Chain inspection'

--- moderate/chain_inspection/main.cpp
+++ moderate/chain_inspection/main.cpp
@@ -50,7 +50,9 @@ public:
                 if (m_start != -1) {
                     return false;
                 }
-                getUnsignedInteger(line, position, left);
+                if (!getUnsignedInteger(line, position, left)) {
+                    return (line == "BEGIN-END");
+                }
                 m_start = left;
 
             } else {
@@ -111,90 +113,6 @@ bool inspectChain(const string &line) {
     return inspector.isValid(line);
 }
 
-bool paranoidInspection(const string &line) {
-    unordered_map<unsigned int, unsigned int> forward;
-    unordered_map<unsigned int, unsigned int> backward;
-    unsigned int m_start = -1;
-    unsigned int m_end = -1;
-
-    size_t position = 0;
-    unsigned int left;
-    unsigned int right;
-
-    while (position < line.length()) {
-        if (line[position] == 'B') {
-            if (m_start != -1) {
-                return false;
-            }
-            getUnsignedInteger(line, position, left);
-            m_start = left;
-
-        } else {
-            getUnsignedInteger(line, position, left);
-            position++;
-
-            if (line[position] == 'E') {
-                if (m_end != -1) {
-                    return false;
-                }
-                m_end = left;
-            } else {
-                getUnsignedInteger(line, position, right);
-
-                if (forward.find(left) != forward.end()) {
-
-                    return false;
-                }
-                if (backward.find(right) != backward.end()) {
-                    return false;
-                }
-
-                forward.emplace(left, right);
-                backward.emplace(right, left);
-            }
-        }
-
-        while (position < line.length() && line[position] != ';') {
-            position++;
-        }
-        position++;
-    }
-
-    if ((m_end == -1) || (m_start == -1)) {
-        return false;
-    }
-
-    unsigned int count = 0;
-    unsigned int index = m_start;
-
-    while ((index != m_end) && (count <= forward.size())) {
-        auto it = forward.find(index);
-        if (it == forward.end()) {
-            return false;
-        }
-        index = it->second;
-        count++;
-    }
-
-    if (count != forward.size()) {
-        return false;
-    }
-
-    count = 0;
-    index = m_end;
-
-    while ((index != m_start) && (count <= backward.size())) {
-        auto it = backward.find(index);
-        if (it == backward.end()) {
-            return false;
-        }
-        index = it->second;
-        count++;
-    }
-
-    return count == backward.size();
-}
-
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         return 0;
@@ -208,7 +126,7 @@ int main(int argc, char *argv[]) {
     }
 
     while ( getline(inFile, inputLine)) {
-        cout << (inspectChain(inputLine) ? "GOOD" : "BAD") << " " << (paranoidInspection(inputLine) ? "GOOD" : "BAD") << endl;
+        cout << (inspectChain(inputLine) ? "GOOD" : "BAD") << endl;
     }
 
     return 0;
